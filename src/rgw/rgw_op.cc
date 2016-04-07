510fcb143bd8a08cae0f0a8079f69f3627139f8f
Adam Kupczyk <akupczyk@mirantis.com>
2016-04-07 16:01:59 +0200

    Changed hash calculation logic. Changed RGWPutObjProcessor interface with regard to hash calculation.
    Signed-off-by: Adam Kupczyk (akupczyk@mirantis.com)
    
    Conflicts:
    	src/rgw/rgw_op.cc
    	src/rgw/rgw_rados.cc

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2937,8 +2937,7 @@ void RGWPutObj::execute()
       orig_data = data;
     }
 
-    op_ret = put_data_and_throttle(processor, data, ofs,
-				  (need_calc_md5 ? &hash : NULL), need_to_wait);
+    op_ret = put_data_and_throttle(processor, data, ofs, need_to_wait);
     if (op_ret < 0) {
       if (!need_to_wait || op_ret != -EEXIST) {
         ldout(s->cct, 20) << "processor->thottle_data() returned ret="
@@ -2968,7 +2967,7 @@ void RGWPutObj::execute()
         goto done;
       }
 
-      op_ret = put_data_and_throttle(processor, data, ofs, NULL, false);
+      op_ret = put_data_and_throttle(processor, data, ofs, false);
       if (op_ret < 0) {
         goto done;
       }
@@ -3192,7 +3191,7 @@ void RGWPostObj::execute()
        break;
 
      hash.Update((const byte *)data.c_str(), data.length());
-     op_ret = put_data_and_throttle(processor, data, ofs, &hash, false);
+     op_ret = put_data_and_throttle(processor, data, ofs, false);
 
      ofs += len;
 
